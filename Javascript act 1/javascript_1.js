42827ccfc06e2a3d43ebc1e7a73236b640cd0de6
Carlos Patiño <49404440+carlosrjpatino@users.noreply.github.com>
2021-07-12 16:36:18 -0300

    Add files via upload

---
+++ Javascript act 1/javascript_1.js
@@ -0,0 +1,7 @@
+
+  primera_variable = 40;
+  segunda_variable = 16;
+  sumatotal = primera_variable + segunda_variable;
+  document.write(
+    "<h1> El resultado de la suma de las dos variables es:<h1/>" + sumatotal
+  );
